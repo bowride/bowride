f22fa640fcf4c0cbf47c1020f4ab036face60202
Derek sola <Dsola@hawaii.edu>
2020-04-23 12:51:02 -1000

    merging all pages

--- app/imports/ui/pages/GoogleMaps.jsx
+++ app/imports/ui/pages/GoogleMaps.jsx
@@ -6,11 +6,11 @@ import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 /** A simple static component to render some text for the landing page. */
 class GoogleMaps extends React.Component {
 
-  /*render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }*/
-
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
     return (
         <div className="ui container" style={{ width: '70vw', height: '70vh', margin:'50px' }}>
           <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCeFS8T27RlCxMzV641cxQmBLh0nDo4fIA'}
