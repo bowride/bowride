9f1e5363c45bacaf09264bcf56fdc3031dd599bf
jake <hawaii_jake@outlook.com>
2020-04-22 17:31:18 -1000

    Update Review.js

--- app/imports/api/Review/Review.js
+++ app/imports/api/Review/Review.js
@@ -2,6 +2,7 @@ import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
 
+/** Define a Mongo collection to hold the data. */
 const Review = new Mongo.Collection('Review');
 
 /** Define a schema to specify the structure of each document in the collection. */
