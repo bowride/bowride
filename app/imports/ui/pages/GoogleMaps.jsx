8ddac9032b4bb7c4522a632fdd8a9d331507b394
Derek sola <Dsola@hawaii.edu>
2020-04-30 02:18:20 -1000

    update to google maps to include onClick events for markers

--- app/imports/ui/pages/GoogleMaps.jsx
+++ app/imports/ui/pages/GoogleMaps.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Header, Loader } from 'semantic-ui-react';
 import GoogleMapReact from 'google-map-react';
 
+//design for marker
 const Marker = ({ text }) => (
     <div style={{
       color: 'white',
@@ -18,10 +19,34 @@ const Marker = ({ text }) => (
     </div>
 );
 
+// map options/design map
+function createMapOptions (maps) {
+  return {
+    panControl: true,
+    mapTypeControl: false,
+    scrollwheel: true,
+    onClick: true,
+    styles: [{ stylers: [{ 'saturation': -100 }, { 'gamma': 1.0 }, { 'lightness': 4 }, { 'visibility': 'on' }] }]
+  }
+}
+
+const renderMarkers = (map, maps) => {
+  // use map and maps objects
+};
+
 export default class SimpleMap extends Component {
 
+  // onClick event for markers
+  _onChildClick = (key, childProps) => {
+    alert('Clicked Marker');
+  };
+
+
+  //render map as <GoogleMapReact> and markers as <Marker>
   render() {
     return (
+
+
 
         <Grid columns={2} style={{ height: '90vh', width: '100vw', backgroundColor: 'green' }}>
           <Grid.Column>
@@ -30,6 +55,10 @@ export default class SimpleMap extends Component {
                 bootstrapURLKeys={{ key: 'AIzaSyDxXxFiIPFfY3-PdFOuEa9gR4hjICcpwZA' }}
                 defaultCenter={{ lat: 21.298872, lng: -157.817204 }}
                 defaultZoom={17}
+                options={createMapOptions}
+                onChildClick={this._onChildClick}
+                yesIWantToUseGoogleMapApiInternals={ true }
+                onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
             >
 
               <Marker
